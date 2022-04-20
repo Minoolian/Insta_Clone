0ab7dd75e6ed4957a1b65f3ff1cced8bcd6f0123
Minoolian <mw.lee@kakao.com>
2022-04-20 19:44:31 +0900

    fix: gitignore 수정

--- reference_code/clone_instargram-master/src/main/java/moon/clone/instargram/domain/comment/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package moon.clone.instargram.domain.comment;
-import moon.clone.instargram.domain.post.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-    void deleteCommentsByPost(Post post);
-}
